a57313266f1dad0d86f6e45b9b8ae179933f96f8
Sheng <cyusheng93@gmail.com>
2020-06-15 10:25:04 +0800

    Fixed a bug with site title

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,7 +28,8 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <Header siteTitle="Sheng" />
       <div>
         <main>{children}</main>
         <footer>
